e7d9b1f71497392e4b781f9288ca7ad82c3f9e74
Alec <alec_z17@web.de>
2025-05-08 13:16:35 +0200

    added footer component

---
+++ src/renderer/src/components/MMFooter/MMFooter.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer class="px-4 bg-white">
+        <p class="text-center py-4 text-xs text-gray bg-background-darker">Copyright © 2025 by <span class="font-bold">Alec</span>. All Rights Reserved.</p>
+    </footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="postcss">
+
+</style>
