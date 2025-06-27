83088afb0e8037da012cb317bfcca1152ef9dc9a
Alec <alec_z17@web.de>
2025-06-27 15:45:22 +0200

    fixed audio type

--- src/handler/audio-handler.ts
+++ src/handler/audio-handler.ts
@@ -3,17 +3,47 @@ import { debug } from '../util/logger'
 import { filterUnknownLanguageTracks } from '../util/utils'
 
 export function processAudio(tracks: AudioTrack[]): void {
-    debug('=== ORIGINAL AUDIO TRACKS ===');
+    debug('=== ORIGINAL AUDIO TRACKS ===')
     tracks.forEach((track, i) => {
-        const lang = track.Language || 'unknown';
-        const format = getAudioType(track);
-        const channels = track.Channels || 0;
-        debug(`[${i}] ${lang} ${format} ${channels}ch - "${track.Title}"`);
-    });
+        const lang = track.Language || 'unknown'
+        const format = getAudioType(track)
+        const channels = track.Channels || 0
+        debug(`[${i}] ${lang} ${format} ${channels}ch - "${track.Title}"`)
+    })
 
     filterUnknownLanguageTracks(tracks)
+    sortAudioTracks(tracks)
+    setDefaultAudioTrack(tracks)
+    renameAudioTracks(tracks)
 
     debug('\n')
+    debug('=== FINAL AUDIO TRACKS ===')
+    tracks.forEach((track, i) => {
+        const lang = track.Language || 'unknown'
+        const type = getAudioType(track)
+        const localIndex = track.LOCAL_INDEX
+        debug(`[${i}] ${lang} ${type} - "${track.Title}" (li: ${localIndex})`)
+    })
+    debug('\n')
+}
+
+function sortAudioTracks(tracks: AudioTrack[]): void {
+
+}
+
+function setDefaultAudioTrack(tracks: AudioTrack[]): void {
+    if (tracks.length === 0) return
+
+    tracks.forEach(track => {
+        track.Default = 'No'
+    })
+}
+
+function renameAudioTracks(tracks: AudioTrack[]): void {
+    tracks.forEach(track => {
+        const type = getAudioType(track)
+        track.Title = getAudioTrackName(type);
+    })
 }
 
 function getAudioType(track: AudioTrack): string {
@@ -23,7 +53,7 @@ function getAudioType(track: AudioTrack): string {
     const isAtmos = additionalFeatures.includes('joc')
         || additionalFeatures.includes('16-ch')
         || title.includes('atmos')
-    const channels = track.Channels === 2 ? track.Channels : (track.Channels - 1);
+    const channels = track.Channels === 2 ? track.Channels : (track.Channels - 1)
 
     let name = ''
 
@@ -31,81 +61,103 @@ function getAudioType(track: AudioTrack): string {
     if (codecId.includes('truehd')) {
         if (isAtmos) {
             name = 'truehd_atmos'
+        } else {
+            name = 'truehd'
         }
-        name = 'truehd'
     }
 
     // EAC3 (Dolby Digital Plus)
-    if (codecId.includes('eac3')) {
+    else if (codecId.includes('eac3')) {
         if (isAtmos) {
             name = 'eac3_atmos'
+        } else {
+            name = 'eac3'
         }
-        name = 'eac3'
     }
 
     // AC3 (Dolby Digital)
-    if (codecId.includes('ac3')) {
+    else if (codecId.includes('ac3')) {
         name = 'ac3'
     }
 
     // DTS Varianten
-    if (codecId.includes('dts')) {
+    else if (codecId.includes('dts')) {
         const commercialIfAny = track.Format_Commercial_IfAny.toLowerCase()
 
         if (additionalFeatures.includes('xxl x') || commercialIfAny.includes('dts:x')) {
             name = 'dts_x'
-        }
-        if (codecId.includes('dts-hd ma') || commercialIfAny.includes('dts-hd master audio')) {
+        } else if (codecId.includes('dts-hd ma') || commercialIfAny.includes('dts-hd master audio')) {
             name = 'dts_hd_ma'
-        }
-
-        if (codecId.includes('dts-hd hr') || commercialIfAny.includes('dts-hd high resolution')) {
+        } else if (codecId.includes('dts-hd hr') || commercialIfAny.includes('dts-hd high resolution')) {
             name = 'dts_hd_hr'
+        } else {
+            name = 'dts'
         }
-        name = 'dts'
     }
 
     // AAC
-    if (codecId.includes('aac')) {
+    else if (codecId.includes('aac')) {
         name = 'aac'
     }
     return `${name}_${channels}`
 }
 
-function getAudioTrackName(track: AudioTrack): string {
-    const type = getAudioType(track)
-
+function getAudioTrackName(type: string): string {
     switch (type) {
-        case 'truehd_atmos_7': return 'Dolby TrueHD Atmos 7.1'
-        case 'truehd_atmos_5': return 'Dolby TrueHD Atmos 5.1'
+        case 'truehd_atmos_7':
+            return 'Dolby TrueHD Atmos 7.1'
+        case 'truehd_atmos_5':
+            return 'Dolby TrueHD Atmos 5.1'
 
-        case 'truehd_7': return 'Dolby TrueHD 7.1'
-        case 'truehd_5': return 'Dolby TrueHD 5.1'
-        case 'truehd_2': return 'Dolby TrueHD Stereo'
+        case 'truehd_7':
+            return 'Dolby TrueHD 7.1'
+        case 'truehd_5':
+            return 'Dolby TrueHD 5.1'
+        case 'truehd_2':
+            return 'Dolby TrueHD Stereo'
 
-        case 'eac3_atmos_7': return 'Dolby Digital Plus Atmos 7.1'
-        case 'eac3_atmos_5': return 'Dolby Digital Plus Atmos 5.1'
+        case 'eac3_atmos_7':
+            return 'Dolby Digital Plus Atmos 7.1'
+        case 'eac3_atmos_5':
+            return 'Dolby Digital Plus Atmos 5.1'
 
-        case 'eac3_7': return 'Dolby Digital Plus 7.1'
-        case 'eac3_5': return 'Dolby Digital Plus 5.1'
-        case 'eac3_2': return 'Dolby Digital Plus Stereo'
+        case 'eac3_7':
+            return 'Dolby Digital Plus 7.1'
+        case 'eac3_5':
+            return 'Dolby Digital Plus 5.1'
+        case 'eac3_2':
+            return 'Dolby Digital Plus Stereo'
 
-        case 'ac3_5': return 'Dolby Digital 5.1'
-        case 'ac3_2': return 'Dolby Digital Stereo'
+        case 'ac3_5':
+            return 'Dolby Digital 5.1'
+        case 'ac3_2':
+            return 'Dolby Digital Stereo'
 
-        case 'dts_x_7': return 'DTS:X 7.1'
-        case 'dts_x_5': return 'DTS:X 5.1'
-        case 'dts_hd_ma_7': return 'DTS-HD MA 7.1'
-        case 'dts_hd_ma_5': return 'DTS-HD MA 5.1'
-        case 'dts_hd_ma_2': return 'DTS-HD MA Stereo'
-        case 'dts_hd_hr_7': return 'DTS-HD HR 7.1'
-        case 'dts_hd_hr_5': return 'DTS-HD HR 5.1'
-        case 'dts_hd_hr_2': return 'DTS-HD HR Stereo'
-        case 'dts_5': return 'DTS 5.1'
-        case 'dts_2': return 'DTS Stereo'
+        case 'dts_x_7':
+            return 'DTS:X 7.1'
+        case 'dts_x_5':
+            return 'DTS:X 5.1'
+        case 'dts_hd_ma_7':
+            return 'DTS-HD MA 7.1'
+        case 'dts_hd_ma_5':
+            return 'DTS-HD MA 5.1'
+        case 'dts_hd_ma_2':
+            return 'DTS-HD MA Stereo'
+        case 'dts_hd_hr_7':
+            return 'DTS-HD HR 7.1'
+        case 'dts_hd_hr_5':
+            return 'DTS-HD HR 5.1'
+        case 'dts_hd_hr_2':
+            return 'DTS-HD HR Stereo'
+        case 'dts_5':
+            return 'DTS 5.1'
+        case 'dts_2':
+            return 'DTS Stereo'
 
-        case 'aac_5': return 'AAC 5.1'
-        case 'aac_2': return 'Stereo'
+        case 'aac_5':
+            return 'AAC 5.1'
+        case 'aac_2':
+            return 'Stereo'
     }
     return 'unknown-audio-name'
 }
